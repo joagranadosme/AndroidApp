8b2b2d13b98edc5d6e0e06c7dbd3fa20d44589df
joagranadosme <joagranadosme@unal.edu.co>
2017-07-06 14:48:30 -0500

    Creating project

---
+++ app/src/main/java/com/example/jonathan/evaluacion/BossActivity.java
@@ -0,0 +1,13 @@
+package com.example.jonathan.evaluacion;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BossActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_boss);
+    }
+}
