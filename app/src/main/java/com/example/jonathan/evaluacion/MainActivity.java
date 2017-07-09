de0d362d0c2c2055b16c50e44693685a7418996b
joagranadosme <joagranadosme@unal.edu.co>
2017-07-09 00:06:28 -0500

    Launching list correctly.

--- app/src/main/java/com/example/jonathan/evaluacion/MainActivity.java
+++ app/src/main/java/com/example/jonathan/evaluacion/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.jonathan.evaluacion;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +13,25 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void adminClick(View view){
+        Toast.makeText(view.getContext(), "Admin click", Toast.LENGTH_LONG).show();
+        //Iniciando la actividad
+        Intent intent = new Intent(this, BossActivity.class);
+        startActivity(intent);
+    }
+
+    public void benefClick(View view){
+        Toast.makeText(view.getContext(), "Beneficio click", Toast.LENGTH_LONG).show();
+    }
+
+    public void performanceClick(View view){
+        Toast.makeText(view.getContext(), "Performance click", Toast.LENGTH_LONG).show();
+    }
+
+    public void compenClick(View view){
+        Toast.makeText(view.getContext(), "Compensacion click", Toast.LENGTH_LONG).show();
+    }
+
+
 }
